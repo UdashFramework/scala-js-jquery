1ceebb4cb1d220cbf2f49988e8f0319a6ec5d1d9
ddworak <dawid.dworak@gmail.com>
2019-10-15 06:52:36 +0200

    Set version to 3.0.2

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@
 name := "udash-jquery"
 
 inThisBuild(Seq(
-  version := "3.0.1",
+  version := "3.0.2",
   organization := "io.udash",
 ))
 
--- example/project/Dependencies.scala
+++ example/project/Dependencies.scala
@@ -1,10 +1,8 @@
-import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
-import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
-import sbt._
+
 
 object Dependencies {
   val udashCoreVersion = "0.8.1"
-  val udashJQueryVersion = "3.0.1"
+  val udashJQueryVersion = "3.0.2"
 
   val deps = Def.setting(Seq[ModuleID](
     "io.udash" %%% "udash-core" % udashCoreVersion,
