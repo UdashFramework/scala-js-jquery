e48541737cc55884d07f7c522123e7e9ae0c5b51
ddworak <dawid.dworak@gmail.com>
2020-03-30 11:59:38 +0200

    Simplified init

--- example/src/main/scala/io/udash/demos/jquery/Init.scala
+++ example/src/main/scala/io/udash/demos/jquery/Init.scala
@@ -1,24 +1,13 @@
 package io.udash.demos.jquery
 
 import io.udash.demos.jquery.views.IndexView
-import io.udash.wrappers.jquery._
 import org.scalajs.dom
-import org.scalajs.dom.Element
 
 import scala.scalajs.js.annotation.JSExport
 
 object Init {
 
   @JSExport
-  def main(args: Array[String]): Unit = {
-    jQ((_: Element) => {
-      val appRoot = jQ("#application").get(0)
-      if (appRoot.isEmpty) {
-        dom.console.error("Application root element not found! Check you index.html file!")
-      } else {
-        appRoot.get.appendChild(IndexView.content.render)
-        //applicationInstance.run(appRoot.get)
-      }
-    })
-  }
+  def main(args: Array[String]): Unit =
+    dom.document.querySelector("#application").appendChild(IndexView.content.render)
 }
